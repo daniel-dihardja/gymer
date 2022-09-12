7671c9996421d1f7be651402cca92c4f85b9abf1
Daniel Dihardja <daniel@dihardja.de>
2022-09-12 20:03:12 +0200

    add mysql typeorm and users module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from "./users/user.entity";
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'user',
+    password: 'user',
+    database: 'test',
+    entities: [User],
+    synchronize: true,
+    dropSchema: true
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/users/user.entity.ts
+++ api/src/users/user.entity.ts
@@ -1,1 +1,17 @@
-export class UserEntity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 45 })
+    email:string;
+
+    @Column({ length: 45})
+    password:string;
+
+    @Column()
+    isActive:boolean;
+}
